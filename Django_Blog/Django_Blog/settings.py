8d21e13fd08defb248a1a60bb0d84d4b1950f6dd
Shivam Agrawal <43890874+s4shivam@users.noreply.github.com>
2021-03-01 23:34:33 +0530

    debug = false

--- Django_Blog/Django_Blog/settings.py
+++ Django_Blog/Django_Blog/settings.py
@@ -23,7 +23,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = '5@a-*_04#5)xmqc)hxe_dm1uccz!q&sa)@$+!02i$^tjz$s(3e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['b4blog.herokuapp.com', '127.0.0.1']
 
